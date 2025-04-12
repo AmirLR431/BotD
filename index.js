25c64366a81c4067849e42f0e2d676be1316bd0e
AmirLR431 <amir0991028@gmail.com>
2025-04-12 14:00:02 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+// برای خواندن توکن از .env
+require('dotenv').config();
 
+// ایمپورت کتابخانه discord.js
+const { Client, GatewayIntentBits } = require('discord.js');
+
+// ساختن کلاینت دیسکورد
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ]
+});
+
+// وقتی ربات آنلاین شد
+client.once('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+// وقتی کسی پیامی داد
+client.on('messageCreate', message => {
+  if (message.content === 'سلام') {
+    message.reply('سلام داداش!');
+  }
+});
+
+// لاگین کردن ربات
+client.login(process.env.TOKEN);
